78b25aeed0adc68670d2519fb6bd3b49617e83ff
JMSBPP <jmsbpp@hotmail.es>
2025-06-08 12:33:49 -0500

    architecturing proxy patterns

---
+++ designPatterns/proxy/README.md
@@ -0,0 +1,8 @@
+# Proxy Patterns
+
+- Retain state while upgrading the logic.
+
+- If the functionality is needed, then a __new logic__ contract is deployed.
+
+
+
--- designPatterns/upgradable/diamond/src/CustomCurveAMMImmutableDiamond.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.25;
-
-import "./interfaces/ICustomCurve.sol";
-
-contract CustomCurveAMMImmutableDiamond {
-    bytes4 constant MODIFY_LIQUIDITY = ICustomCurve.modifyLiquidity.selector;
-    bytes4 constant SWAP = ICustomCurve.swap.selector;
-}
