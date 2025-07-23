a7485ecee61d34e73cf24e3346aa45ef55c2c152
JMSBPP <jmsbpp@hotmail.es>
2025-07-23 13:00:07 -0500

    testing upgrading to CounterV2

--- designPatterns/proxy/src/CounterV2.sol
+++ designPatterns/proxy/src/CounterV2.sol
@@ -3,43 +3,94 @@ pragma solidity ^0.8.24;
 
 import {UUPSUpgradeable} from "openzeppelin-contracts/contracts/proxy/utils/UUPSUpgradeable.sol";
 import {TimelockControllerUpgradeable} from "openzeppelin-contracts-upgradeable/contracts/governance/TimelockControllerUpgradeable.sol";
-/// @custom:oz-upgrades-from CounterV2
-contract CounterV1 is UUPSUpgradeable, TimelockControllerUpgradeable {
+/// @custom:oz-upgrades-from CounterV1
+contract CounterV2 is UUPSUpgradeable, TimelockControllerUpgradeable {
+    
     mapping(address participant => uint256 count) private _participantCounts;
-    constructor() {
-        _disableInitializers();
-    }
-    function increment(address _participant) public {
-        _participantCounts[_participant] =
-            _participantCounts[_participant] +
-            100;
+    bytes32 upgradeProposalId;
+
+    error InvalidCall__UpgradeTimeHasNotPassed();
+    error InvalidCall__UpgradeAlreadyScheduled();
+    
+    modifier onlyAfterScheduledDate() {
+        if (getOperationState(upgradeProposalId) != OperationState.Waiting)
+            revert InvalidCall__UpgradeTimeHasNotPassed();
+        _;
     }
 
-    function getCount(address _participant) external view returns (uint256) {
-        return _participantCounts[_participant];
+    // modifier onlyForUnsetDoneUpgrades() {
+    //     if (
+    //         getOperationState(upgradeProposalId) != OperationState.Unset ||
+    //         getOperationState(upgradeProposalId) != OperationState.Done
+    //     ) revert InvalidCall__UpgradeAlreadyScheduled();
+    //     _;
+    // }
+
+    function increment() public {
+        _participantCounts[msg.sender] = _participantCounts[msg.sender] + 100;
     }
 
-    function _authorizeUpgrade(
-        address newImplementation
-    ) internal virtual override onlyRole(PROPOSER_ROLE) {
+    function getCount() external view returns (uint256) {
+        return _participantCounts[msg.sender];
+    }
+
+    function scheduleUpgrade(
+        uint256 delay
+    )
+        external
+        onlyRole(PROPOSER_ROLE)
+        returns (bytes32 id)
+    {
+        id = _scheduleUpgrade(delay);
+    }
+
+    function _scheduleUpgrade(
+        uint256 delay
+    )
+        internal
+        onlyRole(PROPOSER_ROLE)
+        returns (bytes32 id)
+    {
         schedule(
-            newImplementation,
+            address(this),
             uint256(0x00),
             msg.data[:0],
             bytes32(0x00),
             bytes32(0x00),
-            getMinDelay()
+            delay
+        );
+        upgradeProposalId = hashOperation(
+            address(this),
+            uint256(0x00),
+            msg.data[:0],
+            bytes32(0x00),
+            bytes32(0x00)
+        );
+        id = upgradeProposalId;
+    }
+
+    function _authorizeUpgrade(
+        address newImplementation
+    )
+        internal
+        virtual
+        override
+        onlyRole(PROPOSER_ROLE)
+        onlyAfterScheduledDate
+    {}
+
+    function initialize(address _admin) public virtual initializer {
+        address[] memory proposers = new address[](1);
+        proposers[0] = _admin;
+        __TimelockController_init(
+            uint256(1800),
+            proposers,
+            new address[](0),
+            _admin
         );
     }
 
-    function initialize(
-        address _admin,
-        address _proxy
-    ) public virtual initializer {
-        address[] memory proposers = new address[](1);
-        address[] memory executors = new address[](1);
-        proposers[0] = _admin;
-        executors[0] = _proxy;
-        __TimelockController_init(uint256(1800), proposers, executors, _admin);
+    function setProxyAsExcecutor(address _proxy) public {
+        _grantRole(EXECUTOR_ROLE, _proxy);
     }
 }
--- designPatterns/proxy/test/BuggyUUPSProxyTest.t.sol
+++ designPatterns/proxy/test/BuggyUUPSProxyTest.t.sol
@@ -3,3 +3,80 @@ pragma solidity ^0.8.24;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import {CounterV1} from "../src/CounterV1.sol";
+import {CounterV2} from "../src/CounterV2.sol";
+import {BuggyUUPSProxy} from "../src/BuggyUUPSProxy.sol";
+
+contract BuggyUUPSProxyTest is Test {
+    CounterV1 instance;
+    address proxy;
+    address developerAddress = address(this);
+    address alice = makeAddr("alice");
+    address bob = makeAddr("bob");
+    function setUp() public {
+        vm.startPrank(developerAddress);
+        proxy = Upgrades.deployUUPSProxy(
+            "CounterV1.sol",
+            abi.encodeCall(CounterV1.initialize, (developerAddress))
+        );
+
+        instance = CounterV1(payable(proxy));
+        vm.stopPrank();
+    }
+
+    function test__shouldIncrementToOne() external {
+        vm.startPrank(alice);
+
+        instance.increment();
+        assertEq(1, instance.getCount());
+        vm.stopPrank();
+    }
+
+    function test__fuzz__shouldUpgradeAndIncrementToOneHundred(
+        uint256 _timeStamp,
+        uint256 _delay,
+        uint256 _timeStampDelta
+    ) external {
+        uint256 timeStamp = bound(
+            _timeStamp,
+            uint256(type(uint32).max),
+            uint256(type(uint48).max)
+        );
+
+        uint256 delay = bound(
+            _delay,
+            instance.getMinDelay(),
+            uint256(type(uint32).max)
+        );
+
+        uint256 timeStampDelta = bound(
+            _timeStampDelta,
+            0,
+            uint256(type(uint32).max)
+        );
+
+        vm.warp(timeStamp);
+
+        address counterV1Address = Upgrades.getImplementationAddress(proxy);
+        bytes32 upgradeId = instance.scheduleUpgrade(delay);
+        vm.warp(timeStamp + timeStampDelta);
+
+        vm.startPrank(developerAddress);
+        if (block.timestamp <= timeStamp + delay) {
+            instance.increment();
+            assertEq(1, instance.getCount());
+            vm.expectRevert("InvalidCall__UpgradeTimeHasNotPassed()");
+        }
+
+        Upgrades.upgradeProxy(
+            payable(proxy),
+            "CounterV2.sol",
+            ""
+        );
+
+        CounterV2(payable(address(instance))).increment();
+        assertEq(100, CounterV2(payable(address(instance))).getCount());
+
+        vm.stopPrank();
+    }
+}
