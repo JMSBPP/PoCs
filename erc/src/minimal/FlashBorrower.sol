d5bdc325983bf3429283aada74e24e1ba3d3d90b
JMSBPP <jmsbpp@hotmail.es>
2025-08-03 19:19:55 -0500

    initial commit

---
+++ erc/src/minimal/FlashBorrower.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {FlashLender} from "./FlashLender.sol";
+
+contract FlashBorrower {
+    FlashLender lender;
+
+    constructor(FlashLender _lender) {
+        lender = _lender;
+    }
+
+    function initiateBorrow(uint256 bal) external payable {
+        lender.flashBorrow(bal);
+    }
+
+    function onFlashLoan(uint256 amount) external payable returns (bytes32) {
+        require(msg.sender == address(lender), "only flash lender");
+        // Do something with the borrowed amount
+        // ...
+        (bool ok, ) = address(lender).call{value: amount}("");
+        require(ok, "transfer failed");
+
+        return keccak256("BorrowMoney");
+    }
+}
---
+++ erc/src/minimal/FlashLender.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {FlashBorrower} from "./FlashBorrower.sol";
+contract FlashLender {
+    receive() external payable {}
+
+    function flashBorrow(uint256 bal) external {
+        bytes32 ret = FlashBorrower(msg.sender).onFlashLoan{value: 1 ether}(
+            bal
+        );
+        // expect it back on the same transaction
+        require(ret == keccak256("BorrowMoney"), "invalid response");
+        require(address(this).balance >= bal, "flash loan not paid back");
+    }
+}
